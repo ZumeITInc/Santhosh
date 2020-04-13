ed29b584a0ecfc080f0c83076369b5b510343e52
peace <santhoshkumar0458@gmail.com>
2020-04-14 00:29:25 +0530

    my first commit

---
+++ pythonprograms/arithmetic.py
@@ -0,0 +1,12 @@
+num1 = int(input("enter any number:  "))
+num2 = int(input("enter any number:  "))
+
+sum = num1 + num2
+dif = num1 - num2
+mty = num1 * num2
+dev = num1 / num2
+
+print("sum of two numbers is :", sum)
+print("difference of two numbers is : ", dif)
+print("nultiply of two numbers is: ", mty)
+print("devision of two numbers is: ", dev)
